7c202fbe339a82409adbf17e60ca2ff414c53beb
rogerphilippe@outlook.com <Um9nZXI>
2018-12-12 23:22:40 -0200

    Criado item on click

--- app/src/main/java/br/com/philippesis/listview/MainActivity.java
+++ app/src/main/java/br/com/philippesis/listview/MainActivity.java
@@ -5,8 +5,11 @@ import android.app.ListActivity;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -20,6 +23,14 @@ public class MainActivity extends Activity {
 
         ListView list = (ListView) findViewById(R.id.idLVEquipes);
         list.setAdapter(strAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView txtView = (TextView) findViewById(R.id.idTxtSelect);
+                txtView.setText("ID: "+id+" Posição: "+position);
+            }
+        });
 
     }
 }
